09ff715e032af1e8b08cf28b49d2bd0bda5a19e9
Marcus Salinas <markysalinas12@outlook.com>
2016-03-01 11:06:04 -0600

    Added setup for visual studios if anyone wants to use it.

---
+++ Breakout/Breakout/main.cpp
@@ -0,0 +1,17 @@
+// Breakout.cpp : Defines the entry point for the console application.
+//
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	while(1)
+	{
+		cout << "Marcus Is Awesome.\n";
+	}
+
+	return 0;
+}
+
